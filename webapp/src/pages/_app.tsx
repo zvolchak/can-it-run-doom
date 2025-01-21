bdbd585fcfa028b004477637c32f074f74c60fec
Zach Volchak <zach.bender@protonmail.com>
2025-01-20 21:52:39 -0700

    Deidentified page view tracker (#64)
    page visit deidentified tracking

--- webapp/src/pages/_app.tsx
+++ webapp/src/pages/_app.tsx
@@ -1,8 +1,58 @@
 import "@/src/styles/globals.scss"
-import { Provider, } from "react-redux"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import { Provider, useDispatch, useSelector, } from "react-redux"
+import { v4 as uuidv4 } from 'uuid'
 import { 
+    RootState,
     store,
+    setUserSessionId,
 } from "@/src/store"
+import { analyticsApp, trackPageView } from "@/src/utils/analytics"
+
+
+function UserSessionInit() {
+    const dispatch = useDispatch()
+    const [isSessionCreated, setIsSessionCreated] = useState(false)
+
+    useEffect(() => {
+        if (typeof window === "undefined" || isSessionCreated)
+            return
+
+        dispatch(setUserSessionId(uuidv4()))
+        setIsSessionCreated(true)
+    }, [dispatch, isSessionCreated, setIsSessionCreated])
+
+    return null
+} // UserSessionInit
+
+
+function PageViewAnalytics() {
+    const router = useRouter()
+    const userSessionId: string = useSelector((state: RootState) => state.user?.data?.sessionId) || ""
+    const [isInitPageLoad, setIsInitPageLoad] = useState(false)
+
+    useEffect(() => {
+        if (!userSessionId)
+            return
+
+        const handleRouteChange = (url) => {
+            trackPageView(analyticsApp, url, userSessionId)
+        }
+
+        if (!isInitPageLoad) {
+            handleRouteChange(router.asPath)
+            setIsInitPageLoad(true)
+        }
+        router.events.on("routeChangeComplete", handleRouteChange)
+
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange)
+        }
+    }, [router, userSessionId, isInitPageLoad, setIsInitPageLoad])
+
+    return null
+} // PageViewAnalytics
 
 
 function App({ Component, pageProps }) {
@@ -10,11 +60,14 @@ function App({ Component, pageProps }) {
 
     return (
         <Provider store={store}>
+            <UserSessionInit />
+            <PageViewAnalytics />
             {getLayout(
                 <Component {...pageProps} />
             )}
         </Provider>
     )
 }
+
 
 export default App
